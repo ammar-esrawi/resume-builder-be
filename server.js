1c85ff922b10ad1066039d13d8973f4d2d664eb6
Ammar Esrawi <ammar.magna@gmail.com>
2019-08-21 00:50:17 +0300

    fixes

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const MongoClient = require('mongodb').MongoClient
 const config = require("./config");
 //init the app
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 var db
 
@@ -24,7 +28,33 @@ app.get('/', (req, res) => {
     var cursor = db.collection('profiles').find().toArray(function (err, results) {
         res.json(results)
 
+
     })
+})
+
+app.get('/avatar/:login', (req, res,next) => {
+    var options = {
+        root: __dirname+ '/public',
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+      }
+      var fileName="/assets/avatar/" + req.params.login + ".png";
+    res.sendFile(
+        fileName,
+        options,
+        function (err) {
+            if (err) {
+              next(err)
+            } else {
+              console.log('Sent:',fileName)
+            }
+          }
+    );
+
+
 })
 app.get('/:login', (req, res) => {
     var cursor = db.collection('profiles').find(
@@ -32,16 +62,14 @@ app.get('/:login', (req, res) => {
     )
 
         .toArray(function (err, results) {
-            res.json(results)
+            if (results.length > 0) {
+                res.json(results[0])
+            } else {
+                res.json({
+                    notfound: "Not Found!!"
+                })
+            }
+
 
         })
-})
-
-app.post('/profiles', (req, res) => {
-    db.collection('profiles').save(req.body, (err, result) => {
-        if (err) return console.log(err)
-
-        console.log('saved to database')
-        res.redirect('/')
-    })
 })
