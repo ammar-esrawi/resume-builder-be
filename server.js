31f7cee4feac4318597afd88019235d7d9ea2f81
Ammar Esrawi <ammar.magna@gmail.com>
2019-09-04 03:43:14 +0300

    Lesson 2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const MongoClient = require('mongodb').MongoClient
 const config = require("./config");
 //init the app
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ extended: true }))
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -32,26 +33,25 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/avatar/:login', (req, res,next) => {
+app.get('/avatar/:login', (req, res, next) => {
     var options = {
-        root: __dirname+ '/public',
+        root: __dirname + '/public',
         dotfiles: 'deny',
         headers: {
-          'x-timestamp': Date.now(),
-          'x-sent': true
+            'x-timestamp': Date.now(),
+            'x-sent': true
         }
-      }
-      var fileName="/assets/avatar/" + req.params.login + ".png";
+    }
+    var fileName = "/assets/avatar/" + req.params.login + ".png";
     res.sendFile(
         fileName,
         options,
         function (err) {
             if (err) {
-              next(err)
+                next(err)
             } else {
-              console.log('Sent:',fileName)
             }
-          }
+        }
     );
 
 
@@ -72,4 +72,50 @@ app.get('/:login', (req, res) => {
 
 
         })
+})
+app.post('/login', (req, res) => {
+    console.dir(req.body);
+    var cursor = db.collection('profiles').find(
+        {
+            login: req.body.username,
+            password: req.body.password
+        }
+    )
+
+        .toArray(function (err, results) {
+            if (results.length > 0) {
+                res.json(true)
+            } else {
+                res.json(false)
+            }
+
+
+        })
+})
+app.post('/register', (req, res) => {
+
+    var data = req.body;
+    data.login = data.username;
+    delete data.username;
+    var cursor = db.collection('profiles').find(
+        { login: data.login }
+    )
+
+        .toArray(function (err, results) {
+            if (results.length > 0) {
+                res.json(false);
+            } else {
+                db.collection('profiles').insertOne(data, function (err, res3) {
+                    if (err) throw err;
+                    console.log("Document inserted");
+                    res.json(true);
+
+                });
+            }
+
+
+        })
+
+
+
 })
